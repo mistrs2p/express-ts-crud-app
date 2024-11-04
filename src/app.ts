d7fdac9d450f11ac987316e7f44a87b869775bfb
MistRain <mahdimousavi40@gmail.com>
2024-11-04 17:30:45 +0330

    db connection set

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import authRoutes from "@/routes/authRoutes";
 import userRoutes from "@/routes/userRoutes";
-import connectDB from "@/db";
+import connectDB, { dbStatus } from "@/db";
+import logger from "@/utils/logger";
 
 const app = express();
-app.use(express.json());
 
-connectDB();
+const dbConf = async () => {
+  let tryCon = 3
+  try {
+    const dbState = await connectDB()
+    logger.info(`Database Connection State: ${dbStatus[dbState]}`)
+  } catch (err: any) {
+    logger.error("Database Connection Error: ", err);
+    if (tryCon > 0) {
+      tryCon --
+      dbConf()
+    } else {
+      logger.error("Failed to connect to the database after multiple attempts. Exiting...")
+      process.exit(1)
+    }
+  }
+};
+
+dbConf();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-dotenv.config()
+export const dbStatus = {
+  0: "disconnected",
+  1: "connected",
+  2: "connecting",
+  3: "disconnecting",
+  99: "uninitialized",
+};
+dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_CONNECTION_STRING as string);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(
+      process.env.DB_CONNECTION_STRING as string
+    );
+    // console.log(conn.connection.readyState)
+    // console.log(conn.connection);
+    // console.log(`MongoDB connected: ${conn.connection.host}`);
+    return conn.connection.readyState;
   } catch (error) {
     console.error(`Error: ${(error as Error).message}`);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
